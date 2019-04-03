dfa86e04ed6eec77078027d2a312f9d0d78588ea
Feruza12 <f.latipova@student.inha.uz>
2019-04-03 16:39:23 +0500

    search added

--- js/common.js
+++ js/common.js
@@ -4,33 +4,37 @@ $(function() {
     loop:true,
     margin:10,
     dots: true,
-    // nav: true,
     responsiveClass:true, 
     autoplay:true,
     navigation:true,
     autoplayTimeout:3000,
     autoplayHoverPause:true,    
-    // animateOut: 'fadeOut',
     responsive:{
         0:{
             items:1,
             dots: true,
             dotsEach: true,
-            // nav:true
         },
         600:{
             items:1,
             dots: true,
              dotsEach: true,
-            // nav:false
         },
         1000:{
             items:1,
             dots: true,
              dotsEach: true,
-            // nav:true,
             loop:true		
         }
     }
-})
+    })
+
+    $("#search_button").click(function(){
+        $(this).toggleClass("times");
+        $("#search-form").slideToggle(500);
+    });
+     $(".navbar-toggler").click(function(){
+        $(".bar_icon").toggleClass("close");
+    });
+
 });
